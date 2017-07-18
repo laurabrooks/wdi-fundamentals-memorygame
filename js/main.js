3231b9dd2a0c0736d8a17b1c014ffe028c961ae6
Brooks <dtf725@dca9047648fd.cof.ds.capitalone.com>
2017-07-18 16:09:39 -0400

    updated js to include a function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.")
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
+  } else if (cardsInPlay.length > 2) {
+    console.log("More than two cards in play");
   }
 }
+
+
+var flipCard = function(cardID) {
+  console.log("User flipped " + cards[cardID])
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
